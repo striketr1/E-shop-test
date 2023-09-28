96b58b79789c1efa2d4bc8e15eee2d9370e4a796
Stanislav Vančura <144792244+striketr1@users.noreply.github.com>
2023-09-28 15:07:58 +0200

    ok

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
-document.querySelector(".obrazek");
+
+let a = document.querySelectorAll(".obrazek");
+console.log(a);
+
+let max = 0;
+let b = document.querySelectorAll(".cena");
+
+for (let i = 0; i < b.length; i++) {
+    let c = b[i].textContent;
+    let split = c.substring(10, 90);
+    let cislo = +split;
+
+    if (cislo > max) {
+        max = cislo;
+    }
+    console.log(cislo);
+}
+console.log("Maximální cena je " + max);
+
+
