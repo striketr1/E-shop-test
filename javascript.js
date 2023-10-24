8d3ec598a933f01f566d479f26f6094cf237ce72
Stanislav Vančura <144792244+striketr1@users.noreply.github.com>
2023-10-24 11:54:13 +0200

    něco

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-
 let a = document.querySelectorAll(".obrazek");
 console.log(a);
 
@@ -15,6 +14,34 @@ for (let i = 0; i < b.length; i++) {
     }
     console.log(cislo);
 }
-console.log("Maximální cena je " + max);
+console.log("Maximální cena v $ je " + max);
 
+function odstran(){
+    let para = document.querySelectorAll("article.obrazky >div");
+    for (const i of para) {
+        i.remove();
+    }
+}
+odstran();
 
+let cham = document.querySelectorAll("article.obrazky >div");
+
+let bForward = document.getElementById("forward");
+bForward.addEventListener("click", forward);
+function forward(){
+    for(let i; i < cham.length; i++){
+        for(let j; j < 9;j++){
+        $("article.obrazky").append(cham[i]);
+        }
+    }
+}
+
+let bBack = document.getElementById("back");
+bBack.addEventListener("click", back)
+function back(){
+    for(let i; i < cham.length; i++){
+        for(let j; j < 9;j++){
+        $("article.obrazky").append(cham[i]);
+        }
+    }
+}
